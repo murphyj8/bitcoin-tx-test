61053d9d992e50ec7160ce85c195e9a716bcd61d
John Murphy <j.murphy@nchain.com>
2023-04-05 11:33:03 +0100

    added timing measurements & outputs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 
 
-use std::io::Cursor;
-use bitcoin_tx_test::util::decode_hexstr;
+use std::time::{Duration, SystemTime};
+use std::sync::mpsc;
+use average::MeanWithError;
+
 use bitcoin_tx_test::config;
 use bitcoin_tx_test::key;
 
+use cpu_time::ThreadTime;
 use sv::script::Script;
-use sv::util::Serializable; 
-use sv::messages::{OutPoint,Tx, TxIn, TxOut, Payload};
-use sv::util::{Hash160, Hash256};
+use sv::messages::{OutPoint,Tx, TxIn, TxOut};
+use sv::util::Hash256;
 use sv::transaction::sighash::{sighash, SigHashCache, SIGHASH_ALL, SIGHASH_FORKID};
 use sv::transaction::generate_signature;
-use sv::transaction::p2pkh::{create_lock_script, create_unlock_script};
+use sv::transaction::p2pkh::{create_unlock_script};
 //use threadpool::ThreadPool;
 use num_cpus; 
 use std::thread;
@@ -46,8 +48,8 @@ impl BitcoinTxInfo{
         tx
     }
 }
-fn create_tx(tx_info: Arc<BitcoinTxInfo>){
-    //println!("{}", tx_info.priv_key.get_p2pkh());
+fn create_tx(tx_info: Arc<BitcoinTxInfo>) -> f64 {
+    let thread_start = ThreadTime::now(); 
     let vins: Vec<TxIn> = vec![TxIn {
         prev_output: OutPoint {
             hash: Hash256::decode(&tx_info.tx_hash).unwrap(),
@@ -70,50 +72,46 @@ fn create_tx(tx_info: Arc<BitcoinTxInfo>){
         lock_time: 0,
     };
 
-       // Sign transaction
-       let mut cache = SigHashCache::new();
-       let sighash_type = SIGHASH_ALL | SIGHASH_FORKID;
+    // Sign transaction
+    let mut cache = SigHashCache::new();
+    let sighash_type = SIGHASH_ALL | SIGHASH_FORKID;
+
+    let sighash = sighash(
+        &tx,
+        0,
+        //&change_script_pub_key_bytes.0,
+        &tx_info.script_pub_key.0,
+        tx_info.amt_sats as i64,
+        sighash_type,
+        &mut cache,
+    )
+    .unwrap();
+
+    let signature = generate_signature(
+        &tx_info.priv_key.get_private_key().to_bytes().try_into().unwrap(),
+        //&self.private_key.to_bytes().try_into().unwrap(),
+        &sighash,
+        sighash_type,
+    )
+    .unwrap();
+
+    tx.inputs[0].unlock_script =
+            create_unlock_script(&signature, &tx_info.priv_key.get_public_key().to_bytes().try_into().unwrap());
    
-       let sighash = sighash(
-           &tx,
-           0,
-           //&change_script_pub_key_bytes.0,
-           &tx_info.script_pub_key.0,
-           tx_info.amt_sats as i64,
-           sighash_type,
-           &mut cache,
-       )
-       .unwrap();
-   
-       let signature = generate_signature(
-           &tx_info.priv_key.get_private_key().to_bytes().try_into().unwrap(),
-           //&self.private_key.to_bytes().try_into().unwrap(),
-           &sighash,
-           sighash_type,
-       )
-       .unwrap();
-   
-       tx.inputs[0].unlock_script =
-               create_unlock_script(&signature, &tx_info.priv_key.get_public_key().to_bytes().try_into().unwrap());
-   
-       //dbg!(tx);
-       //let tx_hex = bitcoin_if::tx_as_hexstr(&tx);
-       //println!("{}", tx_hex);
+    //dbg!(tx);
+    //let tx_hex = bitcoin_if::tx_as_hexstr(&tx);
+    //println!("{}", tx_hex);
+    let cpu_time = thread_start.elapsed();
+    cpu_time.as_nanos() as f64
 
 }
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");
-    let tx_string = "0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600";
-
-    let bytes = decode_hexstr(tx_string).unwrap();
-
-    let tx_var : Tx = Tx::read(&mut Cursor::new(&bytes)).unwrap();
-
-    println!("{:?}", tx_var);
-
+    // start the timer 
+    let now = SystemTime::now();
+    
     // load the toml file
     println!("Loading Config toml file {}", "./data/config.toml"); 
     let conf = config::read_config("./data/config.toml").unwrap();
@@ -121,30 +119,57 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:?}", conf);
 
     let tx_info = Arc::new(BitcoinTxInfo::new(&conf));
+    let mut thread_timed_vec = Vec::<f64>::new();
 
     let loop_count = conf.testparams.iteration_count;
-    let num_threads =  num_cpus::get_physical(); 
+    let mut num_threads =  num_cpus::get_physical(); 
+    if conf.testparams.thread_count != 0{
+        num_threads = conf.testparams.thread_count as usize;
+    }
      // A vector containing all the JoinHandles for the spawned threads
     let mut fetch_handle: Vec<thread::JoinHandle<()>> = Vec::new();
+    let total_cpu_start = ThreadTime::now();
     for _ in 0..loop_count{
+        let (tx, rx) = mpsc::channel();
         for _ in 0..num_threads{
             let tx_info = Arc::clone(&tx_info);
+            let thread_tx = tx.clone();
             fetch_handle.push(thread::spawn(move || {
-                create_tx(tx_info);
+                //create_tx(tx_info);
+                thread_tx.send(create_tx(tx_info)).unwrap();
             }));
         }
 
-        while let Some(cur_thread) = fetch_handle.pop() {
-            cur_thread.join().unwrap();
+        for _ in 0..num_threads{
+            thread_timed_vec.push(rx.recv().unwrap());
         }
+        while let Some(cur_thread) = fetch_handle.pop() {
+            cur_thread.join().unwrap(); 
+        } 
     }
+
+    let cpu_total_measured = total_cpu_start.elapsed();
+    let a: MeanWithError = thread_timed_vec.iter().copied().collect();
+
+    println!("Average CPU time per tx {:?} for {} transactions", 
+                Duration::from_nanos(a.mean() as u64),
+                num_threads as i32 * loop_count
+            );
+    println!("Total CPU time for {} transactions is {:?} (sum of each thread time)", 
+                num_threads as i32 * loop_count, 
+                Duration::from_nanos(thread_timed_vec.iter().sum::<f64>() as u64)
+            ); 
+    println!("Total CPU time for the process for {} transactions is {:?}",
+                num_threads as i32 * loop_count, 
+                cpu_total_measured); 
+    println!("Total Wall-clock Time for the process for {} transactions is {:?}", 
+                num_threads as i32 * loop_count, 
+                now.elapsed().unwrap()); 
+
     println!("Finishing");
     Ok(())
 }
 
 /*
-    lanuch a thread for each cpu (if the thread_count is zero)
-    loop for each iteration
-    add the timing details
     write the file info
  */
